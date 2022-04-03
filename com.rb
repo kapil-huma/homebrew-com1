4bdf55faae1e5b25f11d5b4fe7c5071df97b20a2
kapil-huma <kapil@huma.ai>
2022-04-03 17:17:25 +0530

    d

---
+++ com.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Com < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/kapil-huma/com/archive/refs/tags/tmux-cssh_1.0.tar.gz"
+  sha256 "8c4aeace3bc3c63f662b4c029a070bca1199ab2ac76752c951debd57d14c1bca"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+	libexec.install "test.sh"
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test com`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
