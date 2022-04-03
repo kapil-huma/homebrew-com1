7acb4323c5901a6cd74c4e1d2db628a9133aadce
kapil-huma <101082016+kapil-huma@users.noreply.github.com>
2022-04-03 18:15:00 +0530

    Update com.rb

--- com.rb
+++ com.rb
@@ -13,10 +13,19 @@ class Com < Formula
     sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
   end
 
-
   def install
-	  
-	  virtualenv_install_with_resources(:using => "python@3")
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+    %w[six parsedatetime].each do |r|
+      resource(r).stage do
+        system "python", *Language::Python.setup_install_args(libexec/"vendor")
+      end
+    end
+
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    system "python", *Language::Python.setup_install_args(libexec)
+
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
 end
